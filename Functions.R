c1f79754afe327725b9ec698902830b82e853b3b
luoy25 <53832143+luoy25@users.noreply.github.com>
2020-10-02 18:11:06 -0400

    Some RMD updates

--- Functions.R
+++ Functions.R
@@ -13,3 +13,15 @@ Metadata<-function(df){
                       position = "left")
   
 }
+
+Evaluation <- function(model, y_pred, y_true) {
+  library(MLmetrics)
+  adjr2 <- summary(model)$adj.r.squared
+  mse <- MSE(y_pred, y_true)
+  rmse <- RMSE(y_pred, y_true)
+  mae <- MAE(y_pred, y_true)
+  print(paste0("Adjusted R-squared: ", round(adjr2, 4)))
+  print(paste0("MSE: ", round(mse, 4)))
+  print(paste0("RMSE: ", round(rmse, 4)))
+  print(paste0("MAE: ", round(mae, 4)))
+}
